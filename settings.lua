7483f03c769e507e9de4962950ba783d137756d6
alt-potato <jason.why.zhuang@gmail.com>
2025-12-13 22:09:50 -0500

    Add drone attrition setting

---
+++ settings.lua
@@ -0,0 +1,10 @@
+data:extend({
+    {
+        type = "double-setting",
+        name = "long-range-delivery-drones-patched-drone-attrition-rate",
+        setting_type = "runtime-global",
+        min_value = 0.00,
+        max_value = 1.00,
+        default_value = 1.00,
+    }
+})
